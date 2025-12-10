f826603168331228e3de8c76a84b10fd5ceaac08
zeddlyf <zedd.mee00@gmail.com>
2025-12-11 01:29:27 +0800

    Delete Eyy directory

--- Eyy/app/(tabs)/commuter.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-import LoginScreen from '../logincommuter';
-
-export default function CommuterScreen() {
-  return <LoginScreen />;
-}
-
-// Remove unused styles since we're using LoginScreen's styles
-const styles = StyleSheet.create({}); 
--- Eyy/app/(tabs)/rider.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-import LoginRiderScreen from '../loginrider';
-
-export default function RiderScreen() {
-  return <LoginRiderScreen/>;
-}
-
-// Remove unused styles since we're using LoginScreen's styles
-const styles = StyleSheet.create({}); 
